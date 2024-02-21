2472ac29b99f16cdd85f15ec01fd6a777ed446a4
Rad <pouyan.jahni79@gmail.com>
2024-02-22 00:27:18 +0330

    MakeAggregates

---
+++ OnlineShop.Domain/Aggregates/Product/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Domain.Aggregates.Product
+{
+    internal class Product
+    {
+    }
+}
