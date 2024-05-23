7e470029734e059847b53095e557f6454c2eff99
LifeBooster79 <pouyan.jahani79@gmail.com>
2024-05-23 20:05:09 +0430

    makeUpdateOnHomeController

--- OnlineShop.Backoffice/Controllers/HomeController.cs
+++ OnlineShop.Backoffice/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace OnlineShop.Backoffice.Controllers
 {
+    //Test
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
